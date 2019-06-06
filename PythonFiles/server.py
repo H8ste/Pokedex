61758075470342816222e06da16e3df854d37db4
LynzZen <mathias.s.lynge@gmail.com>
2019-06-06 14:27:37 +0200

    Final push

--- PythonFiles/server.py
+++ PythonFiles/server.py
@@ -1,7 +1,7 @@
 #
 #   Hello World server in Python
 #   Binds REP socket to tcp://*:5555
-#   Expects b"Hello" from client, replies with b"World"
+#   Expects a Pokemon name from client, replies with prediction results
 #
 import os
 import time
@@ -22,43 +22,24 @@ from keras.layers import Dense
 from keras.models import model_from_json
 
 def classify(model, labels, imageClassify):
-    # # construct the argument parse and parse the arguments
-    # ap = argparse.ArgumentParser()
-    # ap.add_argument("-m", "--model", required=False,
-    #     help="path to trained model model")
-    # ap.add_argument("-l", "--labelbin", required=True,
-    #     help="path to label binarizer")
-    # ap.add_argument("-i", "--image", required=True,
-    #     help="path to input image")
-    # args = vars(ap.parse_args())
-
 
     # load the trained convolutional neural network and the label
-    # binarizer
-
-    # load json and create model
-    print('currentpath: ' + os.getcwd())
-    print('path: ' + os.getcwd() + "\\PythonFiles"+ "\\models\\" + model + ".json")
     json_file = open(os.getcwd() + "\\PythonFiles" + "\\models\\" + model + ".json", 'r')
     loaded_model_json = json_file.read()
     json_file.close()
-
     loaded_model = model_from_json(loaded_model_json)
+   
     # load weights into new model
     loaded_model.load_weights(os.getcwd() + "\\PythonFiles" + "\\models\\" + model + ".h5")
-    print("Loaded model from disk")
 
-    # print("[INFO] loading network...")
-    # model = load_model(args["model"])
+
+    # load labels
     lb = pickle.loads(open(labels, "rb").read())
 
 
     # load the image
     os.chdir(basePath)
-    # os.chdir("..")
-    print("current directory: " + os.getcwd())
     os.chdir(os.getcwd() + "/UnityProject/Assets/Resources/Textures/")
-    print("current directory: " + os.getcwd())
     image = cv2.imread((imageClassify + ".jpg"))
     output = image.copy()							
 
@@ -74,28 +55,16 @@ def classify(model, labels, imageClassify):
     idx = np.argmax(proba)
     label = lb.classes_[idx]
 
-    print("possible labels: ", lb.classes_)
-
-    # we'll mark our prediction as "correct" of the input image filename
-    # contains the predicted label text (obviously this makes the
-    # assumption that you have named your testing image files this way)
+    # checks againts the predicted label and actual label given from the file name
     inputFileName = imageClassify
     filename = inputFileName[:-3]
-    print("filename: ", filename)
-    print("found label: ", label)
-    #  refind returns -1 if strin doens't contain argument provided
+
+    #  refind returns -1 if string doens't contain argument provided
     correct = "correct" if filename.lower().rfind(label) != -1 else "incorrect"
 
-    # build the label and draw the label on the image
+    # build the label
     label = "{}: {:.2f}% ({})".format(label, proba[idx] * 100, correct)
-    # output = imutils.resize(output, width=450)
-    # cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,
-        # 0.6, (0, 255, 0), 2)
 
-    # show the output image
-    print("[INFO] {}".format(label))
-    # cv2.imshow(filename, output)
-    # cv2.waitKey(0)
     return label
 
 context = zmq.Context()
@@ -104,24 +73,15 @@ socket.bind("tcp://*:5555")
 print ("server running on port 5555")
 basePath = os.getcwd()
 while True:
-
     os.chdir(basePath)
+   
     #  Wait for next request from client
     message = socket.recv_string()
     print("message from server: ", message)
     print("printing results")
     results = classify("pokedex", os.getcwd() + "\\PythonFiles\\" + "labels/pokedex.pickle", message)
-    # print(test)
 
-    #  In the real world usage, you just need to replace time.sleep() with
-    #  whatever work you want python to do.
-    time.sleep(1)
     os.chdir(basePath)
 
-    #  Send reply back to client
-    #  In the real world usage, after you finish your work, send your output here
     socket.send_string(results)
-
-    print("i'm done")
-
     
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Pokedex
-A machine learning bases project in which a player can move around and identify pokemon found in the world based on a trained dataset provided by bing.
+A machine learning based project in which a player can move around and identify pokemon found in the world based on a trained dataset.
+
+The project is run by first running the server.py with a python enviorment which has all the dependencies specified in req.txt installed.
+
+Then the Unity project is run in Unity.
